8f97a784b90fe6083e04f7153dc69cf66a9b8f89
andreasj00 <andbjon@stud.ntnu.no>
2024-04-07 16:33:13 +0200

    added aim trainer code

--- aim_training.py
+++ aim_training.py
@@ -2,3 +2,10 @@ import pyautogui as pag
 import webbrowser
 import time
 webbrowser.open("https://humanbenchmark.com/tests/aim")
+
+time.sleep(2) # Wait for browser to open
+levels = 31 # 30 levels
+
+for i in range(levels):
+    (x, y) = pag.locateCenterOnScreen('aimdot.png', confidence=0.3, region=(400, 200, 1540, 820)) # This resolution is very ish
+    pag.click(x, y)
